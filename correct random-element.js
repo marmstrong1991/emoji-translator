b4ae37b1357015e4e9aa613e668f9a9ebf265aa9
marmstrong1991 <79330700+marmstrong1991@users.noreply.github.com>
2021-07-27 15:47:17 -0400

    Create correct random-element.js

---
+++ correct random-element.js
@@ -0,0 +1,29 @@
+const button = document.queryselector('submit-button');
+
+function onClick(){
+console.log('on click');
+const input = document.querySelector('#translator-input"[type-text]');
+const inoutValue = input.value;
+const h1 = document.querySelector('#results');
+h1.innerHTML =  inputValue;
+}
+
+button.adEventListener('click',onClick
+
+if(checkedRadio.value === 'encode') {
+h1.innerHTML = encode(inputValue) ;
+}
+else if (checkedRadio.value === 'translate') {
+h1.innerHTML = madlib (inputValue);
+}
+else if (checkedRadio.value === 'translate') {
+h1.innerHTML = madlib (inputValue);
+h1.innerHtml = result;
+}
+else if (checkedRadio.value === 'search')
+{ const results = search(inputValue);
+ for ( const result of results) {
+   const p = document.createElement('p')
+   }
+
+}                       
